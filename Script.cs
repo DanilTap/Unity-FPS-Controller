98002236a1350bd95c05a8387bffa167889de4bd
Innokentie <69416693+Innokentie@users.noreply.github.com>
2022-06-12 18:46:47 +0900

    Update Script.cs

--- Script.cs
+++ Script.cs
@@ -1,28 +1,68 @@
-// Connect the script to the object with the RigitBody class
-
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
-public class Moving : MonoBehaviour{
-
+public class Script: MonoBehaviour{
+	[SerializeField] protected GameObject playerHead;
 	private Rigidbody rb;
-	public float PlayerSpeed;
-	public float MouseSpeed = 1f;
 
+	public bool invertVerticalLook;
+	public bool inGround;
+	public float jumpAmount;
+	public float MoveSpeed;
+	public float MouseSpeed;
 
 	void Start(){
 		rb = GetComponent<Rigidbody>();
 		Cursor.lockState = CursorLockMode.Locked;
-		PlayerSpeed = 6f;
+		MoveSpeed = 400f;
+		MouseSpeed = 100f;
+		jumpAmount = 2000f;
+		invertVerticalLook = false;
 	}
-
 
 	void FixedUpdate(){
-		float MoveHor = Input.GetAxis("Horizontal");
-		float MoveVer = Input.GetAxis("Vertical");
-		Vector3 MoveVector = new Vector3(MoveHor, 0.0f, MoveVer);
-
-		rb.AddForce(MoveVector * PlayerSpeed); 
+		MovementLogic();
+		MouseLookLogic();
+		JumpLogic();
 	}
 
+	// Methods
+	private void MovementLogic(){
+		float MoveHor = Input.GetAxis("Horizontal");
+		float MoveVer = Input.GetAxis("Vertical");
+
+		Vector3 move = transform.right * MoveHor + transform.forward * MoveVer;
+		rb.AddForce(move * MoveSpeed);
+	}
+
+	private void MouseLookLogic(){
+		float MouseX = Input.GetAxis("Mouse X");
+		float MouseY = Input.GetAxis("Mouse Y");
+
+		float verticalLookDelta = (MouseY * MouseSpeed) * Time.deltaTime;
+		float horizontalLookDelta = (MouseX * MouseSpeed) * Time.deltaTime;
+		float desiredVerticalLookDelta = (invertVerticalLook) ? verticalLookDelta : -verticalLookDelta; 
+
+		transform.Rotate(new Vector3(0.0f, horizontalLookDelta, 0.0f));
+		playerHead.transform.Rotate(new Vector3(desiredVerticalLookDelta, 0.0f, 0.0f));
+	}
+
+	private void JumpLogic(){
+		if (Input.GetAxis("Jump") > 0){
+			if (inGround){
+				rb.AddForce(Vector3.up * jumpAmount);
+			}
+		}
+	}
+
+	private void OnCollisionEnter(Collision collision){
+		if (collision.gameObject.tag == "Ground"){
+			inGround = true;
+		}
+	}
+
+	private void OnCollisionExit(Collision collision){
+		if (collision.gameObject.tag == "Ground"){
+			inGround = false;
+		}
+	}
 }
